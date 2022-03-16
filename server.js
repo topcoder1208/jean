60558d5fc0d8f8de6c64922f25fb5d47a88f2ffa
Top Coder <61309193+topcoder1208@users.noreply.github.com>
2022-03-17 00:28:14 +0300

    updated

--- server.js
+++ server.js
@@ -1,22 +1,9 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-const fs = require('fs')
-// Configuration
-const PORT = process.env.PORT || '3007';
-const HOST = process.env.HOST || "localhost";
+app.use(express.static(path.join(__dirname, '/build')));
+app.get('*', (req, res) => res.sendFile(__dirname + '/build/index.html'));
 
-// Init Middleware
-app.use(express.json());
-
-app.use(express.static('build'));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-});
-
-// const server = websockets(app);
-// server.listen(PORT, HOST, () => console.log(`Server started on port ${PORT}`));
-app.listen(PORT, HOST, () => console.log(`Server started on port ${PORT}`));
+const PORT = process.env.PORT || 3007;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
